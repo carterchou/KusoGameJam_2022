40ef62aaf9a8de0267e02c5166a306bdd63703b9
FireCat <carter870808@gmail.com>
2022-06-19 15:21:20 +0800

    menu

---
+++ Assets/QuickTake.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickTake : MonoBehaviour
+{
+    public Image hpBar;
+    public GameObject[] supportChara;
+
+    void Update()
+    {
+		if (hpBar != null) {
+            hpBar.fillAmount = 1 / 1;
+        }
+    }
+
+	private void Start() {
+		chooseMode.SelectCharaIdx = 0;
+		if (supportChara != null && supportChara.Length > 0 && chooseMode.SelectCharaIdx >= 0 && chooseMode.SelectCharaIdx <= 7) {
+			foreach (GameObject chara in supportChara) {
+				chara.SetActive(false);
+			}
+			supportChara[chooseMode.SelectCharaIdx].SetActive(true);
+		}
+
+	}
+}
